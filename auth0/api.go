f7f2d22686eca23f43f5f118bb6dbffdfd812791
Abdul Dakkak <abduld@wolfram.com>
2017-03-10 18:30:48 +0000

    work

--- auth0/api.go
+++ auth0/api.go
@@ -7,12 +7,14 @@ import (
 )
 
 type Api struct {
+	token   string
 	options Options
 }
 
 type Options struct {
-	URL               string
-	Token             string
+	Domain            string
+	ClientID          string
+	ClientSecret      string
 	DefaultConnection string
 }
 
@@ -20,13 +22,19 @@ type Option func(*Options)
 
 func URL(s string) Option {
 	return func(o *Options) {
-		o.URL = s
+		o.ClientID = s
 	}
 }
 
-func Token(s string) Option {
+func ClientID(s string) Option {
 	return func(o *Options) {
-		o.Token = s
+		o.ClientID = s
+	}
+}
+
+func ClientSecret(s string) Option {
+	return func(o *Options) {
+		o.ClientID = s
 	}
 }
 
@@ -34,21 +42,38 @@ func New(opts ...Option) *Api {
 	return nil
 }
 
+func (api *Api) getToken() error {
+	type req struct {
+		ClientID     string `json:"client_id"`
+		ClientSecret string `json:"client_secret"`
+		Audience     string `json:"audience"`
+		GrantType    string `json:"grant_type"`
+	}
+	url := "https://" + api.options.Domain + "/oauth/token"
+
+}
+
 func (api *Api) Send(method, endpointUrl string, body interface{}) (*http.Response, error) {
+	if api.token == "" {
+		err := api.getToken()
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	jsonStr, err := json.Marshal(body)
 	if err != nil {
 		return nil, err
 	}
 
-	req, err := http.NewRequest(method, api.options.URL+endpointUrl, bytes.NewBuffer(jsonStr))
+	req, err := http.NewRequest(method, api.options.Domain+endpointUrl, bytes.NewBuffer(jsonStr))
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Authorization", "Bearer "+api.options.Token)
+	req.Header.Set("Authorization", "Bearer "+api.options.token)
 	req.Header.Set("Content-Type", "application/json; charset=utf-8")
 
-	client := &http.Client{}
-	return client.Do(req)
+	return http.DefaultClient.Do(req)
 }
 
 func (api *Api) Options() Options {
--- auth0/config.go
+++ auth0/config.go
@@ -1,1 +1,41 @@
 package auth0
+
+import (
+	"github.com/k0kubun/pp"
+	"github.com/rai-project/config"
+	"github.com/rai-project/vipertags"
+)
+
+type auth0Config struct {
+	Provider     string `json:"provider" config:"auth.provider" default:"auth0"`
+	Domain       string `json:"domain" config:"auth.domain"`
+	ClientID     string `json:"-" config:"auth.client_id"`
+	ClientSecret string `json:"-" config:"auth.client_secret"`
+}
+
+var (
+	Config = &auth0Config{}
+)
+
+func (auth0Config) ConfigName() string {
+	return "Auth0"
+}
+
+func (auth0Config) SetDefaults() {
+}
+
+func (a *auth0Config) Read() {
+	vipertags.Fill(a)
+}
+
+func (c auth0Config) String() string {
+	return pp.Sprintln(c)
+}
+
+func (c auth0Config) Debug() {
+	log.Debug("Auth0 Config = ", c)
+}
+
+func init() {
+	config.Register(Config)
+}
