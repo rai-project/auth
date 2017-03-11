d44d856a572e831e6d1d6562f1dfdeb3fd7a3a1d
Abdul Dakkak <abduld@wolfram.com>
2017-03-11 13:54:54 +0000

    split options and make firstname/lastname be title case

--- base.go
+++ base.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"context"
 	"io/ioutil"
 
 	passlib "gopkg.in/hlandau/passlib.v1"
@@ -16,75 +15,6 @@ import (
 
 type ProfileBase struct {
 	ProfileOptions `json:"-" toml:"profile"`
-}
-
-type ProfileOptions struct {
-	Firstname   string          `json:"firstname" toml:"firstname,omitempty"`
-	Lastname    string          `json:"lastname" toml:"lastname,omitempty"`
-	Username    string          `json:"username" toml:"username"`
-	Email       string          `json:"email" toml:"email"`
-	AccessKey   string          `json:"access_key" toml:"access_key"`
-	SecretKey   string          `json:"secret_key" toml:"secret_key"`
-	Password    string          `json:"password" toml:"-"`
-	ProfilePath string          `json:"-" toml:"-"`
-	AppSecret   string          `json:"-" toml:"-"`
-	Context     context.Context `json:"-" toml:"-"`
-}
-
-type ProfileOption func(*ProfileOptions)
-
-func ProfilePath(s string) ProfileOption {
-	return func(o *ProfileOptions) {
-		o.ProfilePath = s
-	}
-}
-
-func Username(s string) ProfileOption {
-	return func(o *ProfileOptions) {
-		o.Username = s
-	}
-}
-
-func Firstname(s string) ProfileOption {
-	return func(o *ProfileOptions) {
-		o.Firstname = s
-	}
-}
-
-func Lastname(s string) ProfileOption {
-	return func(o *ProfileOptions) {
-		o.Lastname = s
-	}
-}
-
-func Password(s string) ProfileOption {
-	return func(o *ProfileOptions) {
-		o.Password = s
-	}
-}
-
-func Email(s string) ProfileOption {
-	return func(o *ProfileOptions) {
-		o.Email = s
-	}
-}
-
-func AccessKey(s string) ProfileOption {
-	return func(o *ProfileOptions) {
-		o.AccessKey = s
-	}
-}
-
-func SecretKey(s string) ProfileOption {
-	return func(o *ProfileOptions) {
-		o.SecretKey = s
-	}
-}
-
-func AppSecret(s string) ProfileOption {
-	return func(o *ProfileOptions) {
-		o.AppSecret = s
-	}
 }
 
 func NewProfileBase(iopts ...ProfileOption) (*ProfileBase, error) {
