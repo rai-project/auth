227aa4301b2eb49fba3bb37119c1067008ee29ab
Abdul Dakkak <abduld@wolfram.com>
2017-03-12 12:56:17 +0000

    add provider helper

---
+++ provider/provider.go
@@ -0,0 +1,22 @@
+package provider
+
+import (
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/rai-project/auth"
+	"github.com/rai-project/auth/auth0"
+	"github.com/rai-project/auth/secret"
+)
+
+func New(opts ...auth.ProfileOption) (auth.Profile, error) {
+	provider := auth.Provider(strings.ToLower(auth.Config.Provider))
+	switch provider {
+	case auth.Auth0Provider:
+		return auth0.NewProfile(opts...)
+	case auth.SecretProvider:
+		return secret.NewProfile(opts...)
+	default:
+		return nil, errors.Errorf("the auth provider %v specified is not supported", provider)
+	}
+}
