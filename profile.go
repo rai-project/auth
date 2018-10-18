71d3379b17fc741d157ebc96d6e314638eedb135
Abdul Dakkak <abduld@wolfram.com>
2018-10-18 10:40:18 -0500

    start adding and extending comments

--- profile.go
+++ profile.go
@@ -2,23 +2,37 @@ package auth
 
 import "github.com/rai-project/acl"
 
-// Provider ...
 type Provider string
 
-// Auth0Provider ...
+// Providers
 const (
 	Auth0Provider    Provider = "auth0"
 	DatabaseProvider Provider = "database"
 	SecretProvider   Provider = "secret"
 )
 
-// Profile ...
+// Profiles are in effect users for rai
+// each user has a profile that's backed
+// by a profile provider
 type Profile interface {
-	Info() ProfileBase
+	// Create a profile using the provider specified
 	Create() error
+	// Delete a profile
 	Delete() error
+	// Information on the profile
+	Info() ProfileBase
+	// Verify the profile by looking
+	// at the provider and checking if the
+	// credentials match
 	Verify() (bool, error)
+	// Profile options
 	Options() ProfileOptions
+	// Get the role of the user
+	// roles are essentially permission
+	// groups
 	GetRole() (acl.Role, error)
+	// Find a profile by an email. The
+	// profile is found by querying the
+	// profile provier
 	FindByEmail() error
 }
